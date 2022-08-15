b9124962539f86be0311a7cd7215b72f3e291617
Eronan <animeftw@live.co.uk>
2022-08-15 16:37:36 +1000

    Add project files.

---
+++ FECipher/FEMainCharacter.cs
@@ -0,0 +1,45 @@
+﻿using IGamePlugInBase;
+
+namespace FECipher
+{
+    public class FEMainCharacter : IDeck
+    {
+        FECard[] cardList;
+
+        public FEMainCharacter(FECard[] formatCardlist)
+        {
+            this.cardList = formatCardlist;
+        }
+
+        public string Name { get => "maincharacter"; }
+
+        public string Label { get => "Main Character"; }
+
+        public int ExpectedDeckSize { get => 1; }
+
+        public bool ValidateAdd(DeckBuilderCard card, IEnumerable<DeckBuilderCard> deck)
+        {
+            if (deck.Count() > 0) { return false; }
+            FECard feCard = this.cardList.First(listCard => listCard.ID == card.CardID);
+            return deck.Count() == 0 && feCard != null && feCard.cost == "1";
+        }
+
+        public string[] ValidateDeck(IEnumerable<DeckBuilderCard> deck)
+        {
+            if (deck.Count() != 1)
+            {
+                return new string[1] { "You must choose a card to be your Main Character, and only have 1 Main Character." };
+            }
+            else
+            {
+                FECard feCard = this.cardList.First(listCard => listCard.ID == deck.First().CardID);
+                if (feCard != null && feCard.cost != "1")
+                {
+                    return new string[1] { "Your Main Character must be a 1 Cost Card." };
+                }
+            }
+
+            return new string[0];
+        }
+    }
+}
